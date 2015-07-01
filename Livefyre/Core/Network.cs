81fcbe312b3c5b6492c56dba2f4658fe0902f7aa
mwebb <mwebb@livefyre.com>
2015-07-01 12:17:59 -0700

    Prepping for Site.cs

--- Livefyre/Core/Network.cs
+++ Livefyre/Core/Network.cs
@@ -29,233 +29,217 @@ namespace Livefyre.Core
             this.data = data;
         }
 
-        public static Network Init(String name, String key) {
+        public static Network Init(String name, String key)
+        {
             NetworkData data = new NetworkData(name, key);
             return new Network(data/* David: not nec to use reflection here if desired */);
         }
 
-          /**
-             * Updates the user sync url. Makes an external API call. 
-             * http://answers.livefyre.com/developers/user-auth/remote-profiles/#ping-for-pull.
-             * 
-             * @param urlTemplate the url template to set.
-          */
-    /**
-     * Updates the user sync url. Makes an external API call. 
-     * http://answers.livefyre.com/developers/user-auth/remote-profiles/#ping-for-pull.
-     * 
-     * @param urlTemplate the url template to set.
-     */
-    public void SetUserSyncUrl(String url) {
-        Precondition.CheckNotNull(url, String.Format("urlTemplate does not contain {0}", ID));
-
-        try 
-	    {	        
-            // fix ref
-            String postData = String.Format("{0}", Domain.quill(this));
-            // fix ref
-            postData = String.Format(postData + "{0}", this.buildLivefyreToken());
-            //add Params
-            // make params vars/members
-            // actor_token
-            // pull_profile_url
-            byte[] postBytes = Encoding.UTF8.GetBytes(postData);
-
-            Uri uri = new Uri(url);
-            WebRequest request = WebRequest.Create(uri);
-            request.ContentType = "application/x-www-form-urlencoded";
-
-            Stream dataStream = request.GetRequestStream();
-            dataStream.Write(postBytes, 0, postBytes.Length);
-			WebResponse response = request.GetResponse();
-            Console.WriteLine (((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.
-            dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.
-            Console.WriteLine (responseFromServer);
-            // Clean up the streams.
-            reader.Close();
-            dataStream.Close();
-            response.Close();
-	    }
-	    catch (Exception e)
-	    {
-		    throw e;
-	    }
-
-    }
-
-    
-    /**
-     * Informs Livefyre to fetch user information based on the user sync url. Makes an external API call.
-     * 
-     * @param userId the userId for the user to sync
-     * @return true if the sync was successful.
-     */
-    public Network SyncUser(String userId) {
-        Precondition.CheckNotNull(userId);
-        
-        //fix ref
-        //make configurable/pull out mutable api v3_0 key
-        string url = String.Format("{0}/api/v3_0/user/{1}/refresh", Domain.quill(this), userId);
-        // pull bits out of try
-        try 
-	    {	        
-            // fix ref
-            String postData = String.Format("{0}", Domain.quill(this));
-            // fix ref
-            postData = String.Format(postData + "{0}", this.buildLivefyreToken());
-            //add Params
-            // make params vars/members
-            // lftoken
-            byte[] postBytes = Encoding.UTF8.GetBytes(postData);
-
-            Uri uri = new Uri(url);
-            WebRequest request = WebRequest.Create(uri);
-            request.ContentType = "application/x-www-form-urlencoded";
-
-            Stream dataStream = request.GetRequestStream();
-            dataStream.Write(postBytes, 0, postBytes.Length);
-			WebResponse response = request.GetResponse();
-            Console.WriteLine (((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.
-            dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader (dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.
-            Console.WriteLine (responseFromServer);
-            // Clean up the streams.
-            reader.Close();
-            dataStream.Close();
-            response.Close();
-
-
-            // capture/throw a server error code too
-
-            return this;
-	    }
-	    catch (Exception e)
-	    {
-		    throw e;
-	    }
- 
-    }
-    
-    /**
-     * Generates a user auth system token.
-     * 
-     * @return a default system token
-     */
-    public string buildLivefyreToken() {
-        return buildUserAuthToken(DEFAULT_USER, DEFAULT_USER, DEFAULT_EXPIRES);
-    }
-    
-    /**
-     * Generates a user auth token passed on the params passed in. This method serializes the params
-     * and signs the String with the Network key.
-     * 
-     * @param userId the user id for this token.
-     * @param displayName the display name for this token.
-     * @param expires when the token should expire from the time of its creation in seconds.
-     * @return String
-     */
-    public String buildUserAuthToken(String userId, String displayName, Double expires) {
-        Regex pattern = new Regex(ALPHA_DASH_UNDER_DOT_REGEX);
-        // should we use ms code contracts?
-        // requires a runtime chekcing settings
-        Precondition.CheckNotNull(userId);
-        Precondition.CheckNotNull(displayName);
-        Precondition.CheckNotNull(expires);
-
-       // var/mem this error message
-        if (pattern.IsMatch(userId)) {
-        
-        /*
-        // change this MS type
-        Map<String, Object> claims = ImmutableMap.<String, Object>of(
-                "domain", data.getName(),
-                "user_id", userId,
-                "display_name", displayName,
-                "expires", getExpiryInSeconds(expires)
-            );
-
-        return SerializeAndSign(claims, data.getKey());
+        /**
+           * Updates the user sync url. Makes an external API call. 
+           * http://answers.livefyre.com/developers/user-auth/remote-profiles/#ping-for-pull.
+           * 
+           * @param urlTemplate the url template to set.
         */
+        /**
+         * Updates the user sync url. Makes an external API call. 
+         * http://answers.livefyre.com/developers/user-auth/remote-profiles/#ping-for-pull.
+         * 
+         * @param urlTemplate the url template to set.
+         */
+        public void SetUserSyncUrl(String url)
+        {
+            Precondition.CheckNotNull(url, String.Format("urlTemplate does not contain {0}", ID));
 
+            try
+            {
+                // fix ref
+                String postData = String.Format("{0}", Domain.quill(this));
+                // fix ref
+                postData = String.Format(postData + "{0}", this.buildLivefyreToken());
+                //add Params
+                // make params vars/members
+                // actor_token
+                // pull_profile_url
+                byte[] postBytes = Encoding.UTF8.GetBytes(postData);
 
-        } else {
-            throw new Exception(String.Format("userId is not valid. be sure the userId matches the following pattern: {0}", 
-                    ALPHA_DASH_UNDER_DOT_REGEX));
+                Uri uri = new Uri(url);
+                WebRequest request = WebRequest.Create(uri);
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(postBytes, 0, postBytes.Length);
+                WebResponse response = request.GetResponse();
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                // Get the stream containing content returned by the server.
+                dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer);
+                // Clean up the streams.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
         }
-        
-    }
 
-    /**
-     * Checks to see if the passed in system token is still valid.
-     * 
-     * @param lfToken the system token to validate
-     * 
-     * @return true if the token is still valid.
-     */
-    public boolean validateLivefyreToken(String lfToken) {
-        CheckNotNull(lfToken);
 
-        JsonObject json = LivefyreUtil.decodeJwt(lfToken, data.getKey());
-        return json.get("domain").getAsString().compareTo(data.getName()) == 0
-            && json.get("user_id").getAsString().compareTo("system") == 0
-            && json.get("expires").getAsLong() >= Calendar.getInstance().getTimeInMillis()/1000L;
-    }
-    
-    /**
-     * Constructs a new Site object based on the parameters passed in.
-     * 
-     * @param siteId the id for the Site.
-     * @param siteKey the secret key for the Site.
-     * @return Site
-     */
-    public Site getSite(String siteId, String siteKey) {
-        return Site.init(this, siteId, siteKey);
-    }
-    
-    public String getUrn() {
-        return "urn:livefyre:"+data.getName();
-    }
-    
-    public String getUrnForUser(String user) {
-        return getUrn()+":user="+user;
-    }
-    
-    public String getNetworkName() {
-        return data.getName().split("\\.")[0];
-    }
+        /**
+         * Informs Livefyre to fetch user information based on the user sync url. Makes an external API call.
+         * 
+         * @param userId the userId for the user to sync
+         * @return true if the sync was successful.
+         */
+        public Network SyncUser(String userId)
+        {
+            Precondition.CheckNotNull(userId);
 
-    public Boolean isSsl() {
-        return ssl;
-    }
+            //fix ref
+            //make configurable/pull out mutable api v3_0 key
+            string url = String.Format("{0}/api/v3_0/user/{1}/refresh", Domain.quill(this), userId);
+            // pull bits out of try
+            try
+            {
+                // fix ref
+                string postData = String.Format("{0}", Domain.quill(this));
+                // fix ref
+                postData = String.Format(postData + "{0}", this.buildLivefyreToken());
+                //add Params
+                // make params vars/members
+                // lftoken
+                byte[] postBytes = Encoding.UTF8.GetBytes(postData);
 
-    public void setSsl(Boolean ssl) {
-        this.ssl = ssl;
-    }
+                Uri uri = new Uri(url);
+                WebRequest request = WebRequest.Create(uri);
+                request.ContentType = "application/x-www-form-urlencoded";
 
-    public NetworkData getData() {
-        return data;
-    }
-    
-    public void setData(NetworkData data) {
-        this.data = data;
-    }
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(postBytes, 0, postBytes.Length);
+                WebResponse response = request.GetResponse();
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                // Get the stream containing content returned by the server.
+                dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer);
+                // Clean up the streams.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
 
-    /* Protected/private methods */
-    private long getExpiryInSeconds(double secTillExpire) {
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        cal.add(Calendar.SECOND, (int) secTillExpire);
-        return cal.getTimeInMillis() / 1000L;
+
+                // capture/throw a server error code too
+
+                return this;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        /**
+         * Generates a user auth system token.
+         * 
+         * @return a default system token
+         */
+        public string buildLivefyreToken()
+        {
+            return buildUserAuthToken(DEFAULT_USER, DEFAULT_USER, DEFAULT_EXPIRES);
+        }
+
+        /**
+         * Generates a user auth token passed on the params passed in. This method serializes the params
+         * and signs the String with the Network key.
+         * 
+         * @param userId the user id for this token.
+         * @param displayName the display name for this token.
+         * @param expires when the token should expire from the time of its creation in seconds.
+         * @return String
+         */
+        public String BuildUserAuthToken(String userId, String displayName, Double expires)
+        {
+            Regex pattern = new Regex(ALPHA_DASH_UNDER_DOT_REGEX);
+            // should we use ms code contracts?
+            // requires a runtime checking setting
+            Precondition.CheckNotNull(userId);
+            Precondition.CheckNotNull(displayName);
+            Precondition.CheckNotNull(expires);
+
+            // var/mem this error message
+            if (pattern.IsMatch(userId))
+            {
+
+                /*
+                // change this MS type
+                Map<String, Object> claims = ImmutableMap.<String, Object>of(
+                        "domain", data.getName(),
+                        "user_id", userId,
+                        "display_name", displayName,
+                        "expires", getExpiryInSeconds(expires)
+                    );
+
+                return SerializeAndSign(claims, data.getKey());
+                */
+                return "";
+
+            }
+            else
+            {
+                throw new Exception(String.Format("userId is not valid. be sure the userId matches the following pattern: {0}",
+                        ALPHA_DASH_UNDER_DOT_REGEX));
+            }
+
+        }
+
+        /**
+         * Checks to see if the passed in system token is still valid.
+         * 
+         * @param lfToken the system token to validate
+         * 
+         * @return true if the token is still valid.
+         */
+        public bool ValidateLivefyreToken(String lfToken)
+        {
+            Precondition.CheckNotNull(lfToken);
+
+            // JSON.Net http://www.newtonsoft.com/json lib or MS JSON Serializer?
+
+            /*
+            JsonObject json = LivefyreUtil.decodeJwt(lfToken, data.getKey());
+            return json.get("domain").getAsString().compareTo(data.getName()) == 0
+                && json.get("user_id").getAsString().compareTo("system") == 0
+                && json.get("expires").getAsLong() >= Calendar.getInstance().getTimeInMillis()/1000L;
+             */
+            return false;
+        }
+
+
+
+        /* Protected/private methods */
+        // move to Util
+        private long getExpiryInSeconds(double secTillExpire)
+        {
+            //MS Time
+            /*
+            Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+            cal.add(Calendar.SECOND, (int) secTillExpire);
+            return cal.getTimeInMillis() / 1000L;
+            */
+            return 199430;
+        }
     }
 }
 
--- Livefyre/Core/Site.cs
+++ Livefyre/Core/Site.cs
@@ -7,5 +7,146 @@ namespace Livefyre.Core
 {
     class Site
     {
+
+        package com.livefyre.core;
+
+import com.livefyre.model.SiteData;
+import com.livefyre.type.CollectionType;
+import com.livefyre.validator.ReflectiveValidator;
+
+public class Site implements LfCore {
+    private Network network;
+    private SiteData data;
+
+    public Site(Network network, SiteData data) {
+        this.network = network;
+        this.data = data;
+    }
+
+    public static Site init(Network network, String siteId, String siteKey) {
+        SiteData data = new SiteData(siteId, siteKey);
+        return new Site(network, ReflectiveValidator.validate(data));
+    }
+
+    /* Default collection type */
+    public Collection buildBlogCollection(String title, String articleId, String url) {
+        return buildCollection(CollectionType.BLOG, title, articleId, url);
+    }
+
+    public Collection buildChatCollection(String title, String articleId, String url) {
+        return buildCollection(CollectionType.CHAT, title, articleId, url);
+    }
+
+    public Collection buildCommentsCollection(String title, String articleId, String url) {
+        return buildCollection(CollectionType.COMMENTS, title, articleId, url);
+    }
+    
+    public Collection buildCountingCollection(String title, String articleId, String url) {
+        return buildCollection(CollectionType.COUNTING, title, articleId, url);
+    }
+    
+    public Collection buildRatingsCollection(String title, String articleId, String url) {
+        return buildCollection(CollectionType.RATINGS, title, articleId, url);
+    }
+    
+    public Collection buildReviewsCollection(String title, String articleId, String url) {
+        return buildCollection(CollectionType.REVIEWS, title, articleId, url);
+    }
+    
+    public Collection buildSidenotesCollection(String title, String articleId, String url) {
+        return buildCollection(CollectionType.SIDENOTES, title, articleId, url);
+    }
+    
+    /**
+     * Creates and returns a Collection object. Be sure to call createOrUpdate() on it to inform Livefyre to
+     * complete creation and any updates.
+     * 
+     * Options accepts a map of key/value pairs for your Collection. Some examples are 'tags',
+     * 'type', 'extensions', 'tags', etc. Please refer to
+     * http://answers.livefyre.com/developers/getting-started/tokens/collectionmeta/ for more info.
+     *
+     * @param type the type of the collection.
+     * @param articleId the articleId for the collection.
+     * @param title title for the collection.
+     * @param url url for the collection.
+     * 
+     * @return Collection
+     */
+    public Collection buildCollection(CollectionType type, String title, String articleId, String url) {
+        return Collection.init(this, type, title, articleId, url);
+    }
+    
+    //build different collection types here
+
+    /* Getters/Setters */
+    public String getUrn() {
+        return network.getUrn() + ":site=" + data.getId();
+    }
+    
+    public Network getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(Network network) {
+        this.network = network;
+    }
+
+    public SiteData getData() {
+        return data;
+    }
+
+    public void setData(SiteData data) {
+        this.data = data;
+    }
+}
+
+
+        // MOVE TO SITE.cs
+        /**
+         * Constructs a new Site object based on the parameters passed in.
+         * 
+         * @param siteId the id for the Site.
+         * @param siteKey the secret key for the Site.
+         * @return Site
+         */
+        public Site getSite(String siteId, String siteKey)
+        {
+            return Site.init(this, siteId, siteKey);
+        }
+
+        public String getUrn()
+        {
+            return "urn:livefyre:" + data.getName();
+        }
+
+        public String getUrnForUser(String user)
+        {
+            return getUrn() + ":user=" + user;
+        }
+
+        public String getNetworkName()
+        {
+            return data.getName().split("\\.")[0];
+        }
+
+        public Boolean isSsl()
+        {
+            return ssl;
+        }
+
+        public void setSsl(Boolean ssl)
+        {
+            this.ssl = ssl;
+        }
+
+        public NetworkData getData()
+        {
+            return data;
+        }
+
+        public void setData(NetworkData data)
+        {
+            this.data = data;
+        }
     }
 }
