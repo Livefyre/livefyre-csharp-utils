171c1471d9fc36fc25faf20c2b65c919af181963
mwebb <mwebb@livefyre.com>
2015-07-09 10:58:16 -0700

    Cursory cursoring

--- Livefyre/Cursor/TimelineCursor.cs
+++ Livefyre/Cursor/TimelineCursor.cs
@@ -3,9 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
+using Livefyre.Core;
+
 namespace Livefyre.Cursor
 {
-    class TimelineCursor
+
+    /**
+     * TimelineCursor is an entity that keeps track of positions in timelines for different resources.
+     * When created via a Network object, it will keep track of timelines at the network level, and
+     * likewise at the site level if created via a Site object.
+     */
+    public class TimelineCursor
     {
+        private LFCore core;
+        private CursorData data;
+
+        public TimelineCursor(LfCore core, CursorData data)
+        {
+            this.core = core;
+            this.data = data;
+        }
+
+        public static TimelineCursor init(LfCore core, String resource, Integer limit, Date startTime)
+        {
+            CursorData data = new CursorData(resource, limit, startTime);
+            return new TimelineCursor(core, ReflectiveValidator.validate(data));
+        }
+
+        /**
+         * Returns the next set of events in the timeline from the cursorTime.
+
+         * @return JSONObject
+         */
+        public JsonObject next()
+        {
+            JsonObject responseData = PersonalizedStream.getTimelineStream(this, true);
+            JsonObject cursor = responseData.getAsJsonObject("meta").getAsJsonObject("cursor");
+
+            data.setNext(cursor.get("hasNext").getAsBoolean());
+            data.setPrevious(!cursor.get("next").isJsonNull());
+            if (data.isPrevious())
+            {
+                data.setCursorTime(cursor.get("next").getAsString());
+            }
+            return responseData;
+        }
+
+        /**
+         * Returns the previous set of events in the timeline from the cursorTime.
+
+         * @return JSONObject
+         */
+        public JsonObject previous()
+        {
+            JsonObject responseData = PersonalizedStream.getTimelineStream(this, false);
+            JsonObject cursor = responseData.getAsJsonObject("meta").getAsJsonObject("cursor");
+
+            data.setPrevious(cursor.get("hasPrev").getAsBoolean());
+            data.setNext(!cursor.get("prev").isJsonNull());
+            if (data.isNext())
+            {
+                data.setCursorTime(cursor.get("prev").getAsString());
+            }
+            return responseData;
+        }
+
+        public LfCore getCore()
+        {
+            return core;
+        }
+
+        public void setCore(LfCore core)
+        {
+            this.core = core;
+        }
+
+        public CursorData getData()
+        {
+            return data;
+        }
+
+        public void setData(CursorData data)
+        {
+            this.data = data;
+        }
     }
+
 }
--- Livefyre/Model/CursorData.cs
+++ Livefyre/Model/CursorData.cs
@@ -5,7 +5,75 @@ using System.Text;
 
 namespace Livefyre.Model
 {
-    class CursorData
-    {
+
+public class CursorData {
+    // may need to be special type
+    protected static readonly string DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    //DateTime.Now.ToString("yyyyMMddHHmmss");
+
+    private string resource;
+    private string cursorTime;
+    private bool next = false;
+    private bool previous = false;
+    private int limit;
+    
+    public CursorData(string resource, int limit, Date startTime) {
+        // figure out this-ness 
+       // DATE_FORMAT.SetTimeZone(TimeZone.GetTimeZone("UTC"));
+        this.resource = resource;
+        this.limit = limit;
+       // this.cursorTime = startTime == null ? null: DATE_FORMAT.format(startTime);
     }
+
+    public string GetResource() {
+        return resource;
+    }
+
+    public CursorData SetResource(string resource) {
+        this.resource = resource;
+        return this;
+    }
+
+    public string GetCursorTime() {
+        return cursorTime;
+    }
+
+    public CursorData SetCursorTime(string newTime) {
+        this.cursorTime = newTime;
+        return this;
+    }
+
+    public CursorData SetCursorTime(DateTime newTime) {
+       // this.cursorTime = DATE_FORMAT.format(newTime);
+        return this;
+    }
+
+    public bool isPrevious() {
+        return previous;
+    }
+
+    public CursorData SetPrevious(bool previous) {
+        this.previous = previous;
+        return this;
+    }
+
+    public bool isNext() {
+        return next;
+    }
+
+    public CursorData SetNext(bool next) {
+        this.next = next;
+        return this;
+    }
+
+    public int GetLimit() {
+        return limit;
+    }
+
+    public CursorData SetLimit(int limit) {
+        this.limit = limit;
+        return this;
+    }
+}
+
 }
